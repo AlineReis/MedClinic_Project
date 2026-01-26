9773a3c5cdf9e8c1daf8a2eca9232023a54154dc
Weydson <weydsonmarinho@gmail.com>
2026-01-25 23:15:57 -0300

    feat: initialize database and add auth registration route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { database } from "@config/database.js";
 import express from "express";
 import routes from "./routes/index.js";
 
@@ -8,6 +9,11 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server online on PORT ${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    await database.initialize();
+    console.log(`Servidor online na PORTA ${PORT}`);
+  } catch (error) {
+    console.error("Falha ao iniciar a aplicação:", error);
+  }
 });
