9bda95bea28cb8fc2cfda2605b43afa1ac59b0d5
Gerson Gomes <gersoncastrogomes95@gmail.com>
2026-01-26 12:24:35 -0300

    Controlador e rota de agendamento

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authRoutes } from "./auth.routes.js";
 import { userRoutes } from "./users.routes.js";
+import { appointmentRoutes } from "./appointment.routes.js";
 
 const router = Router();
 
 router.use("/api/v1/:clinic_id/auth", authRoutes);
 router.use("/api/v1/:clinic_id/users", userRoutes);
+router.use("/api/v1/:clinic_id/appointments", appointmentRoutes);
 
 export default router;
