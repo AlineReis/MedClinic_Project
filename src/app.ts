e91d51ca8b06c8249cf327a779daa8a5fc2bcb7a
José Henrique <castro.andrade@hotmail.com.br>
2026-01-26 06:46:23 -0300

    feat: configure global middlewares and register auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-import { database } from "@config/database.js";
+import { database } from "./config/database.js";
 import express from "express";
 import routes from "./routes/index.js";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const PORT = 3000;
 const app = express();
 
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 app.use(express.json());
 
 app.use(routes);
