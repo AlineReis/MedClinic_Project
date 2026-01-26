af171aed088ce22fe8e583339b7114f86d3900a4
Weydson <weydsonmarinho@gmail.com>
2026-01-25 23:00:28 -0300

    chore: add json body parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express from "express"
-import routes from "./routes/index.js"
+import express from "express";
+import routes from "./routes/index.js";
 
-const PORT = 3000
+const PORT = 3000;
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server online on PORT ${PORT}`)
-})
+  console.log(`Server online on PORT ${PORT}`);
+});
