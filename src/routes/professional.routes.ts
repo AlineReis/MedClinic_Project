da24665e176ca005be08a7f889bc82091206f9b3
Weydson <weydsonmarinho@gmail.com>
2026-01-26 14:55:08 -0300

    refactor(controller): instantiate ProfessionalController with service

--- src/routes/professional.routes.ts
+++ src/routes/professional.routes.ts
@@ -1,9 +1,22 @@
+import { AvailabilityRepository } from "@repositories/availability.repository.js";
+import { ProfessionalRepository } from "@repositories/professional.repository.js";
+import { UserRepository } from "@repositories/user.repository.js";
+import { ProfessionalService } from "@services/professional.service.js";
 import { Router } from "express";
 import { ProfessionalController } from "../controller/professional.controller.js";
 
 const router = Router();
 
-router.post('/', ProfessionalController.register);
-router.get('/:specialty', ProfessionalController.listBySpecialty);
+
+
+const usersRepository = new UserRepository();
+const availabilityRepository = new AvailabilityRepository();
+const professionalRepository = new ProfessionalRepository();
+const professionalService = new ProfessionalService(usersRepository, professionalRepository, availabilityRepository);
+const professionalController = new ProfessionalController(professionalService);
+
+
+router.post('/', professionalController.register);
+router.get('/:specialty', professionalController.listBySpecialty);
 
 export { router as professionalRoutes };
